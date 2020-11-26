5e1d585e9dd00bf6f53e8c20a5cba5e436f05e84
Jesse Feddema <jessef@specialisterren.nl>
2020-11-26 15:32:49 +0100

    Test starts with non-clean localStorage

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,15 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
+describe('localStorage', () => {
+  it('sets localStorage', () => {
+    cy.visit('cypress/integration/setLocalStorage.html')
+    cy.get('body').should(() => {
+      expect(localStorage.getItem('foo')).to.equal('bar')
+    })
+  })
+  it('clears localStorage between tests', () => {
+    cy.visit('cypress/integration/index.html')
+    cy.get('body').should(() => {
+      expect(localStorage.getItem('foo')).to.be.null
+    })
   })
 })
